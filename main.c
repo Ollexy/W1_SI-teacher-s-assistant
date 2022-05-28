456213efc7dda901ea970a9ca1b05899bf34d87f
Rafal Oleksiewicz <70261243+Ollexy@users.noreply.github.com>
2022-05-28 16:45:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,160 +1,234 @@
 #include <stdio.h>
+#include <time.h>
 
-
-void stats();
 void menu();
-void typingGrade(float average);
-void scholarship(float average);
+void gradeCalculation(float average);
+void subjects();
+float enterForPerson();
 
 int main()
 {
 	char choice;
 
-	do {
+	while (1) {
 
 		menu();
 
 		choice = getch();
 
-		float grades[8], sum = 0, average, student[5], math[7], english[7], history[7];
+		//declaring variables
+		float student[5], math[6], english[6], history[6], averageThreeNotes[5], scholarship = 0;
 
 		switch (choice)
 		{
+		case '1':					//choosing a student
+		{
+			printf("Harper Eland\n\n");
+			subjects();
 
-		case '1':
-			printf("Harper Eland\n");
-
-
-				for (int i = 0; i < 7; i++)
-				{
-					printf("Enter %d grade: ", i + 1);
-					scanf_s("%g", grades + i);
-
-					sum += grades[i];
-				}
-				average = sum / 7;
-				printf("%.1f\n", average);
-
-				typingGrade(average);
-				scholarship(average);
-			break;
-		case '2':
-			printf("Celement Hayes\n");
-
-			for (int i = 0; i < 7; i++)
+			choice = getch();
+			if (choice == 'm')		//choosing a subject
 			{
-				printf("Enter %d grade: ", i + 1);
-				scanf_s("%g", grades + i);
-
-				sum += grades[i];
+				math[0] = enterForPerson();		//calculating grade from each subject
 			}
-			average = sum / 7;
-			printf("%.1f\n", average);
-
-			typingGrade(average);
-			scholarship(average);
-			break;
-		case '3':
-			printf("Balinsa Jacobs\n");
-
-			for (int i = 0; i < 7; i++)
+			else if (choice == 'e')
 			{
-				printf("Enter %d grade: ", i + 1);
-				scanf_s("%g", grades + i);
-
-				sum += grades[i];
+				english[0] = enterForPerson();
 			}
-			average = sum / 7;
-			printf("%.1f\n", average);
-
-			typingGrade(average);
-			scholarship(average);
-			break;
-		case '4':
-			printf("Cedric Santos\n");
-
-			for (int i = 0; i < 7; i++)
+			else if (choice == 'h')
 			{
-				printf("Enter %d grade: ", i + 1);
-				scanf_s("%g", grades + i);
-
-				sum += grades[i];
+				history[0] = enterForPerson();
 			}
-			average = sum / 7;
-			printf("%.1f\n", average);
-
-			typingGrade(average);
-			scholarship(average);
-			break;
-		case '5':
-			printf("Fiona Wilcher\n");
-
-			for (int i = 0; i < 7; i++)
+			else if (choice == 's')
 			{
-				printf("Enter %d grade: ", i + 1);
-				scanf_s("%g", grades + i);
-
-				sum += grades[i];
+				averageThreeNotes[0] = (math[0] + english[0] + history[0]) / 3;		//you have to calculate average for every signle
+				printf("Average from 3 subject = %.1f\n", averageThreeNotes[0]);	//subject then you can calculate if student is eligable
+				if (averageThreeNotes[0] > 95)										//for scholarship*/
+					printf("You are eligable for scholarship\n\n");
 			}
-			average = sum / 7;
-			printf("%.1f\n", average);
-
-			typingGrade(average);
-			scholarship(average);
-			break;
-		case '6':
-			stats();
-			break;
-
-		default: printf("There is no such option. Try again with another one.\n");
-
 		}
-	}while (choice != '1','2','3','4','5','6');
+		break;
+		case '2':				
+		{
+			printf("Celement Hayes\n\n");
+			subjects();
+
+			choice = getch();
+			if (choice == 'm')
+			{
+				math[1] = enterForPerson();
+			}
+			else if (choice == 'e')
+			{
+				english[1] = enterForPerson();
+			}
+			else if (choice == 'h')
+			{
+				history[1] = enterForPerson();
+			}
+			else if (choice == 's')
+			{
+				averageThreeNotes[1] = (math[1] + english[1] + history[1]) / 3;
+				printf("Average from 3 subject = %.1f\n", averageThreeNotes[1]);
+				if (averageThreeNotes[1] > 95)
+					printf("You are eligable for scholarship\n\n");
+			}
+		}
+		break;
+		case '3':					//wybor ucznia
+		{
+			printf("Belinsa Jacobs\n\n");
+			subjects();
+
+			choice = getch();
+			if (choice == 'm')
+			{
+				math[2] = enterForPerson();
+			}
+			else if (choice == 'e')
+			{
+				english[2] = enterForPerson();
+			}
+			else if (choice == 'h')
+			{
+				history[2] = enterForPerson();
+			}
+			else if (choice == 's')
+			{
+				averageThreeNotes[2] = (math[2] + english[2] + history[2]) / 3;
+				printf("Average from 3 subject = %.1f\n", averageThreeNotes[2]);
+				if (averageThreeNotes[2] > 95)
+					printf("You are eligable for scholarship\n\n");
+			}
+		}
+		break;
+		case '4':					//wybor ucznia
+		{
+			printf("Cedric Santos\n\n");
+			subjects();
+
+			choice = getch();
+			if (choice == 'm')
+			{
+				math[3] = enterForPerson();
+			}
+			else if (choice == 'e')
+			{
+				english[3] = enterForPerson();
+			}
+			else if (choice == 'h')
+			{
+				history[3] = enterForPerson();
+			}
+			else if (choice == 's')
+			{
+				averageThreeNotes[3] = (math[3] + english[3] + history[3]) / 3;
+				printf("Average from 3 subject = %.1f\n", averageThreeNotes[3]);
+				if (averageThreeNotes[3] > 95)
+					printf("You are eligable for scholarship\n\n");
+			}
+		}
+		break;
+		case '5':					//wybor ucznia
+		{
+			printf("Fiona Wilcher\n\n");
+			subjects();
+
+			choice = getch();
+			if (choice == 'm')
+			{
+				math[4] = enterForPerson();
+			}
+			else if (choice == 'e')
+			{
+				english[4] = enterForPerson();
+			}
+			else if (choice == 'h')
+			{
+				history[4] = enterForPerson();
+			}
+			else if (choice == 's')
+			{
+				averageThreeNotes[4] = (math[4] + english[4] + history[4]) / 3;
+				printf("Average from 3 subject = %.1f\n", averageThreeNotes[0]);
+				if (averageThreeNotes[4] > 95)
+					printf("You are eligable for scholarship\n\n");
+			}
+		}
+		break;
+
+		case '6':
+		{
+			printf("STATS\n");
+			printf("\n");
+			printf("Average math note: %.1f \n");
+			printf("Average english note: \n");
+			printf("Average history note: \n");
+			printf("Highest note: \n");
+			printf("Lowest note: \n");
+			printf("Students with a schoolarship: \n\n");
+			return 0;
+		}
+
+		default: printf("There is no such option. Try again with another one.\n\n");
+			Sleep(500);	/*after 0.5s screen*/
+			system("cls");	/*will be cleared*/
+
+			break;
+		}
+	}
 	return 0;
 }
-void stats()
-{
-	printf("STATS\n");
-	printf("\n");
-	printf("Average math note: \n");
-	printf("Average english note: \n");
-	printf("Average history note: \n");
-	printf("Highest note: \n");
-	printf("Lowest note: \n");
-	printf("Students with a schoolarship: \n");
-}
 void menu()
-{
-	printf("CHOOSE STUDENT\n");
-	printf("---------------\n");
-	printf("(1) Harper Eland\n");
-	printf("(2) Celement Hayes\n");
-	printf("(3) Belinsa Jacobs\n");
-	printf("(4) Cedric Santos\n");
-	printf("(5) Fiona Wilcher\n");
-	printf("(6) Stats\n\n");
-}
-void typingGrade(float average)
-{
-	if (average < 50)
-		printf("Grade: F\n");
-	else if ((average > 50) && (average <= 60))
-		printf("Grade: E\n");
-	else if ((average > 60) && (average <= 70))
-		printf("Grade: D\n");
-	else if ((average > 70) && (average <= 80))
-		printf("Grade: C\n");
-	else if ((average > 80) && (average <= 90))
-		printf("Grade: B\n");
-	else if (average > 90)
-		printf("Grade: A\n");
-	else
-		printf("Invalid!\n");
-}
-void scholarship(float average)
-{
-	if (average > 95)
-		printf("Eligible for scholarship\n\n");
-	else
-		printf("Not eligible for scholarship\n\n");
-}
+	{
+		printf("CHOOSE STUDENT\n");
+		printf("---------------\n");
+		printf("(1) Harper Eland\n");
+		printf("(2) Celement Hayes\n");
+		printf("(3) Belinsa Jacobs\n");
+		printf("(4) Cedric Santos\n");
+		printf("(5) Fiona Wilcher\n");
+		printf("(6) Stats\n\n");
+	}
+void gradeCalculation(float average)
+	{
+		if (average < 50)
+			printf("Grade: F\n\n");
+		else if (average <= 60)
+			printf("Grade: E\n\n");
+		else if (average <= 70)
+			printf("Grade: D\n\n");
+		else if ((average > 70) && (average <= 80))
+			printf("Grade: C\n\n");
+		else if ((average > 80) && (average <= 90))
+			printf("Grade: B\n\n");
+		else if (average > 90)
+			printf("Grade: A\n\n");
+		else
+			printf("Invalid!\n\n");
+
+		Sleep(1000);	/*after 1s screen*/
+		system("cls");	/*will be cleared*/
+	}
+void subjects()
+	{
+		printf("(m) Math\n");
+		printf("(e) English\n");
+		printf("(h) History\n");
+		printf("(s) Scholarship\n\n");
+	}
+float enterForPerson() {
+		float curGrade, sum = 0, average;
+		for (int i = 0; i < 7; i++)
+		{
+			printf("Enter %d grade: ", i + 1);
+			scanf_s("%g", &curGrade);
+
+			sum += curGrade;
+		}
+		average = sum / 7;
+		printf("%.1f\n", average);
+
+		gradeCalculation(average);
+		return average;
+	}
